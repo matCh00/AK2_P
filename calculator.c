eda5a0c7bbe1d0584242065a8ac6adb939591673
matCh00 <mateusz.chalik.g@gmail.com>
2021-05-16 01:18:11 +0200

    comments

--- calculator.c
+++ calculator.c
@@ -9,6 +9,7 @@ double division(double a, double b);
 double square_root(double a);
 double sinus(double a);
 
+// zmienne
 double a = 0.0f;
 double b = 0.0f;
 double result = 0.0f;
@@ -20,8 +21,9 @@ int main() {
 
   int key = 0;
 
-  while(true)
-  {
+
+  while(true) {
+
       printf("\n");
       printf("Dostepne opcje:\n");
       printf("0. Wyjscie\n");
@@ -34,61 +36,76 @@ int main() {
       printf("Wybierz opcje: ");
       scanf("%i", &key);
 
+      // wyjscie z petli
       if(key == 0)
-      {
-          break;
-      }
+        break;
 
+      // wprowadzenie liczb
       printf("(Dla pierwiastka i sinusa rozpatrywane jest a)\n");
       printf("Wpisz a: ");
       scanf("%lf", &a);
       printf("Wpisz b: ");
       scanf("%lf", &b);
 
-      switch(key)
-      {
+
+      switch(key) {
+
           case 1:
           {
               result = addition(a, b);
               printf("Suma: %f\n", result);
               break;
           }
+
           case 2:
           {
               result = subtraction(a, b);
               printf("Roznica: %f\n", result);
               break;
           }
+
           case 3:
           {
               result = multiplication(a, b);
               printf("Iloczyn: %f\n", result);
               break;
           }
+
           case 4:
           {
               if(b != 0)
               {
                   result = division(a, b);
                   printf("Iloraz: %f\n", result);
-              } else
+              }
+              else  // rozwiazany problem z dzieleniem przez 0
               {
                   printf("Nie mozna dzielic przez 0\n");
               }
               break;
           }
+
           case 5:
           {
-              result = square_root(a);
-              printf("Pierwiastek: %f\n", result);
+              if(a >= 0)
+              {
+                result = square_root(a);
+                printf("Pierwiastek: %f\n", result);
+              }
+              else  // rozwiazany problem z pierwiastkowaniem liczb ujemnych
+              {
+                printf("Nie istnieje pierwiastek kwadratowy z liczby ujemnej\n");
+              }
               break;
           }
+
           case 6:
           {
               result = sinus(a);
               printf("Sinus: %f\n", result);
               break;
           }
+
           default:
               printf("Nie ma takiej opcji\n");
               break;
