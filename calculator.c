07c22ff2ad436f1e75d2ae543ef7e2ef7d431254
matCh00 <mateusz.chalik.g@gmail.com>
2021-05-15 18:58:16 +0200

    c file changes

--- calculator.c
+++ calculator.c
@@ -1,30 +1,20 @@
 #include <stdio.h>
+#include <stdlib.h>
 
-void calc();
+// prototypy funkcji
+double addition(double a, double b);
+double subtraction(double a, double b);
+double multiplication(double a, double b);
+double division(double a, double b);
+double square_root(double a);
+double sinus(double a);
 
 
 int main() {
 
   printf("Projekt AK2 - Mateusz Chalik\n\n");
 
-  printf("Podaj dwie liczby typu float, znak('+' '-' '*' '/' '^') oraz sposob zaokraglania(c-cut, u-up, d-down, n-nearest): \n");
-
-
-  calc();
-
-  //int num1, num2;
-  //scanf("%d", &num1);
-  //scanf("%d", &num2);
-  //char key;
-  //scanf("%c", &key);
-
-  //switch(key){
-    //case'+': addition(num1, num2); break;
-    //case'-': printf("2"); break;
-    //case'*': printf("3"); break;
-    //case'/': printf("4"); break;
-    //default: break;
-//  }
+  int key = 0;
 
   return 0;
 }
