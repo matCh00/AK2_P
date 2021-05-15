9e103854b17db0959627e0208e135e7ee0e0a41e
matCh00 <mateusz.chalik.g@gmail.com>
2021-05-16 01:09:28 +0200

    interface in C

--- calculator.c
+++ calculator.c
@@ -16,5 +16,82 @@ int main() {
 
   int key = 0;
 
+  while(true)
+  {
+      printf("\n");
+      printf("Dostepne opcje:\n");
+      printf("0. Wyjscie\n");
+      printf("1. Dodawanie\n");
+      printf("2. Odejmowanie\n");
+      printf("3. Mnozenie\n");
+      printf("4. Dzielenie\n");
+      printf("5. Pierwiastek\n");
+      printf("6. Sinus\n");
+      printf("Wybierz opcje: ");
+      scanf("%i", &key);
+
+      if(key == 0)
+      {
+          break;
+      }
+
+      printf("(Dla pierwiastka i sinusa rozpatrywane jest a)\n");
+      printf("Wpisz a: ");
+      scanf("%lf", &a);
+      printf("Wpisz b: ");
+      scanf("%lf", &b);
+
+      switch(key)
+      {
+          case 1:
+          {
+              result = addition(a, b);
+              printf("Suma: %f\n", result);
+              break;
+          }
+          case 2:
+          {
+              result = subtraction(a, b);
+              printf("Roznica: %f\n", result);
+              break;
+          }
+          case 3:
+          {
+              result = multiplication(a, b);
+              printf("Iloczyn: %f\n", result);
+              break;
+          }
+          case 4:
+          {
+              if(b != 0)
+              {
+                  result = division(a, b);
+                  printf("Iloraz: %f\n", result);
+              } else
+              {
+                  printf("Nie mozna dzielic przez 0\n");
+              }
+              break;
+          }
+          case 5:
+          {
+              result = square_root(a);
+              printf("Pierwiastek: %f\n", result);
+              break;
+          }
+          case 6:
+          {
+              result = sinus(a);
+              printf("Sinus: %f\n", result);
+              break;
+          }
+          default:
+              printf("Nie ma takiej opcji\n");
+              break;
+      }
+  }
+
+  printf("Koniec\n");
+
   return 0;
 }
